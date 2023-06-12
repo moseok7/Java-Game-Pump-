5de418e653bf64f3437ece83a6b42e2241795c51
Moseok kim <dnehftka@naver.com>
2023-06-13 03:10:19 +0900

    first commit

---
+++ src/pump_version_1/PumpGame.java
@@ -0,0 +1,19 @@
+package pump_version_1;
+
+import javax.swing.JFrame;
+
+public class PumpGame extends JFrame {
+	
+	public PumpGame() {
+		setTitle("Pump Game");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		
+		
+	}
+	
+}
